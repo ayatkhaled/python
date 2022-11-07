0a4e264713496dcc230f8e8b7eb87b659c430073
ayatkhaled <71589348+ayatkhaled@users.noreply.github.com>
2022-11-07 17:43:52 +0200

    Add files via upload

--- ITISHOP.py
+++ ITISHOP.py
@@ -6,6 +6,7 @@ print("=========================")
 choise= int(input("Enter your choise "))
 mydic = {}
 my_dict = {'1-apple':70 , '2-banana':50,'3-orange':60}
+dict = {'1-apple':15 , '2-banana':20,'3-orange':17}
 
 if(choise==1):
 	print("hello owner")
@@ -23,30 +24,31 @@ if(choise==1):
 			print("=========================")
 			select= int(input("Enter your choise "))
 			
-			
+			if(select==1):
+				print(my_dict)
 			
 			if(select==2):
 				
 				print(my_dict)
 			if(select==3):
 				
-				print(my_dict)
+				print(dict)
 				sel=int(input("select the item you want to change its price "))
 				if (sel==1):
 					key='1-apple'
 					change= int(input("Enter the new price "))
-					my_dict[key] = change
-					print(my_dict)
+					dict[key] = change
+					print(dict)
 				if (sel==2):
 					key='2-banana'
 					change= int(input("Enter the new price "))
-					my_dict[key] = change
-					print(my_dict)
+					dict[key] = change
+					print(dict)
 				if (sel==3):
 					key='3-orange'
 					change= int(input("Enter the new price "))
-					my_dict[key] = change
-					print(my_dict)
+					dict[key] = change
+					print(dict)
 				
 				
 				
@@ -61,6 +63,29 @@ if(choise==1):
 	
 if(choise==2):
 	print("welcome sir")
+	print("that what we have")
+	print(dict)
+	ch= int(input("chose the item "))
+	if (ch==1):
+		key='1-apple'
+		amount= int(input("Enter the amount "))
+		price=dict.get('1-apple')
+		total=price*amount
+		print(total)
+	
+	if (ch==2):
+		key='2-banana'
+		amount= int(input("Enter the amount "))
+		price=dict.get('2-banana')
+		total=price*amount
+		print(total)
+	if (ch==2):
+		key='3-orange'
+		amount= int(input("Enter the amount "))
+		price=dict.get('3-orange')
+		total=price*amount
+		print(total)	
+	
 if(choise==3):
 	print("Good Bye sir")
 	exit()
