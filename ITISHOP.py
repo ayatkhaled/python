5175c52c60d3cf4d13a4ece8c282450646767c3f
ayatkhaled <71589348+ayatkhaled@users.noreply.github.com>
2022-11-21 16:54:55 +0200

    Add files via upload

--- ITISHOP.py
+++ ITISHOP.py
@@ -1,3 +1,4 @@
+import csv
 print("welcome to our ITI Shop")
 print("1-Are you owner?")
 print("2-Are you coustomer?")
@@ -7,25 +8,36 @@ choise= int(input("Enter your choise "))
 mydic = {}
 my_dict = {'1-apple':70 , '2-banana':50,'3-orange':60}
 dict = {'1-apple':15 , '2-banana':20,'3-orange':17}
+employee={'ahmed':200,'ali':100}
+fired=["tamer","ayat"]
+thislist = ["ali", "amany", "yasser"]
 
 if(choise==1):
 	print("hello owner")
+	userName= (input("Enter your user name "))
+	password= int(input("Enter your password "))
 	while True:
-		userName= (input("Enter your user name "))
-		password= int(input("Enter your password "))
+		
 		if(userName=="ayat" and password==1234):
-			print("welcome MR ayat")
+			print("welcome MISS ayat")
 			print("Tell me how can I help you")
 			print("=========================")
 			print("1-Add New Product")
 			print("2-Show Product")
 			print("3-Change Cost")
-			print("4-Exit")
+			print("4-add new employee")
+			print("5-add new one to fired")
+			print("6-exit")
 			print("=========================")
 			select= int(input("Enter your choise "))
 			
 			if(select==1):
 				print(my_dict)
+				sel=(input("write the new item "))
+				price=int(input("enter its price "))
+				my_dict[sel]=price
+				print(my_dict)
+
 			
 			if(select==2):
 				
@@ -52,7 +64,32 @@ if(choise==1):
 				
 				
 				
-			if(select==4):
+			if (select==4):
+					print(employee)
+					new= input("Enter the name of the new one ")
+
+					salary=int(input("enter his salary "))
+					employee[new]=salary
+					print(employee)
+					my_dict = {"test": 1, "testing": 2}
+					with open('names.csv', 'w') as f:  # You will need 'wb' mode in Python 2.x
+						w = csv.DictWriter(f, employee.keys())
+						w.writeheader()
+						w.writerow(employee)
+			if (select==5):
+					print(fired)
+					fired= input("Enter the name of fired one ")
+					print (fired)
+					thislist.append(fired)
+					print(thislist)
+					with open('fired.csv', 'w') as myfile:
+						wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+						wr.writerow(thislist)
+					
+
+					
+                    					
+			if(select==6):
 				print("Good Bye sir")
 				exit()
 			
@@ -60,31 +97,32 @@ if(choise==1):
 			
 		else:
 			print("ivalid password or user name please try again")
-	
+
 if(choise==2):
 	print("welcome sir")
 	print("that what we have")
 	print(dict)
-	ch= int(input("chose the item "))
-	if (ch==1):
-		key='1-apple'
-		amount= int(input("Enter the amount "))
-		price=dict.get('1-apple')
-		total=price*amount
-		print(total)
+	while True:
+		ch= int(input("chose the item "))
+		if (ch==1):
+			key='1-apple'
+			amount= int(input("Enter the amount "))
+			price=dict.get('1-apple')
+			total=price*amount
+			print(total)
 	
-	if (ch==2):
-		key='2-banana'
-		amount= int(input("Enter the amount "))
-		price=dict.get('2-banana')
-		total=price*amount
-		print(total)
-	if (ch==2):
-		key='3-orange'
-		amount= int(input("Enter the amount "))
-		price=dict.get('3-orange')
-		total=price*amount
-		print(total)	
+		if (ch==2):
+			key='2-banana'
+			amount= int(input("Enter the amount "))
+			price=dict.get('2-banana')
+			total=price*amount
+			print(total)
+		if (ch==3):
+			key='3-orange'
+			amount= int(input("Enter the amount "))
+			price=dict.get('3-orange')
+			total=price*amount
+			print(total)	
 	
 if(choise==3):
 	print("Good Bye sir")
